9a6678cfae4733cc1b23fe824c41178d412701f6
sreeroop <sreeroopsk10@gmail.com>
2021-11-19 01:31:59 +0530

    deleted unwanted files

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
 var indexRouter = require('./routes/index');
@@ -9,14 +7,10 @@ var indexRouter = require('./routes/index');
 var app = express();
 app.use(cors())
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
