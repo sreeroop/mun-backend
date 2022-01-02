57b533e19586153ce85e19752380bafe57655d8a
sreeroop <sreeroopsk10@gmail.com>
2022-01-02 13:37:26 +0000

    cors

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var cors = require('cors')
 var indexRouter = require('./routes/index');
 
 var app = express();
-app.use(cors())
+app.use(cors({
+	origin: ["https://nitcmun.ipfnitc.com"],
+  optionsSuccessStatus: 200
+}))
 // view engine setup
 
 app.use(logger('dev'));
